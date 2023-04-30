6624eb25ccc184921f9c8a95bc4709799b1ed0ce
Sybilus <88580591+Sybilus@users.noreply.github.com>
2023-04-30 02:16:05 +0200

    swapping base of site to the other

--- everything/carousel.js
+++ everything/carousel.js
@@ -1,8 +1,11 @@
+
 const flicking = new Flicking("#flick", {
-align: "center",
+  align: "center",
   renderOnlyVisible: true,
   preventClickOnDrag: true,
   autoResize: true,
   circular:false,
   Touch: true,
 });
+
+
