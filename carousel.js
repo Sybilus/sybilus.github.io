c46ce0730ca439eb7f03113dd5a23bc56ec1edfc
Sybilus <88580591+Sybilus@users.noreply.github.com>
2023-04-30 02:17:25 +0200

    new files for site

---
+++ carousel.js
@@ -0,0 +1,10 @@
+const flicking = new Flicking("#flick", {
+  align: "center",
+  renderOnlyVisible: true,
+  preventClickOnDrag: true,
+  autoResize: true,
+  circular:false,
+  Touch: true,
+});
+
+
